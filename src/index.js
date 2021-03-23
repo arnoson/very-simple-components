616a3c0a05cfb4e900801339b453113a7edde65f
arnoson <hallo@arno-schlipf.info>
2021-03-23 19:14:18 +0100

    Remove events

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import {
-  walkComponent,
-  parseEvents,
-  registerEvents,
-  isString
-} from './utils/utils.js'
+import { walkComponent, isString } from './utils/utils.js'
 
 /**
- * @typedef {({ el: HTMLElement, refs: Refs}) => object?} Component
+ * @typedef {Object<string, HTMLElement | Array<HTMLElement>>} Refs
+ */
+
+/**
+ * @typedef {(el: HTMLElement) => object?} Component
  */
 
 /** @type {Object<string, Component>} */
@@ -25,6 +24,9 @@ const getComponent = el => components[el.dataset.component]
  * @returns {Component}
  */
 export const registerComponent = (name, component) => {
+  if (typeof component !== 'function') {
+    throw new Error(`Component ${name} is not a function.`)
+  }
   components[name] = component
   return component
 }
@@ -32,51 +34,25 @@ export const registerComponent = (name, component) => {
 /**
  * Mount a single component.
  * @param {HTMLElement} el
- * @param {object} [component]
  */
-export const mountComponent = (el, component = null, isChild = false) => {
-  component = component ?? getComponent(el)
+export const mountComponent = (el, isChild = false) => {
+  const component = getComponent(el)
+  if (component) {
+    /* @ts-ignore */
+    el.__very_instance = component(el)
+  }
 
-  // We first store all events without registering them, because we don't know
-  // the refs yet (which we need to create the event handlers' context).
-  const allEvents = []
-  const refs = {}
-
-  // Search for `data-on-...` event definitions and references.
-  // `walkComponent` won't traverse nested components but will execute the
-  // callback a last time on the nested component's root element before
-  // stopping.
-  walkComponent(el, (el, isChildComponent) => {
-    // Add a new reference to the list or group multiple references with the
-    // same name into an array.
-    const { ref } = el.dataset
-    if (ref) {
-      const entry = refs[ref]
-      refs[ref] = entry
-        ? Array.isArray(entry)
-          ? entry.concat(entry, el)
-          : [entry, el]
-        : el
-    }
-
-    if (!isChildComponent) {
-      const events = parseEvents(el)
-      events && allEvents.push(events)
-    }
-  })
-
-  const context = component({ el, refs })
-  registerEvents(allEvents, context)
-
-  // Store the context on the el so we can retrieve it with the `very()` helper
-  // function.
-  /* @ts-ignore */
-  el.__very = context
-
-  // `mountChildComponents` will detect also nested children so we have to call
-  // it only once on the top component.
   if (!isChild) {
     mountChildComponents(el)
+  }
+}
+
+/** @param {HTMLElement} el */
+const mountChildComponents = el => {
+  const elements = el.querySelectorAll('[data-component]')
+  for (let i = 0; i < elements.length; i++) {
+    const el = /** @type {HTMLElement} */ (elements[i])
+    mountComponent(el, true)
   }
 }
 
@@ -85,27 +61,35 @@ export const mountComponent = (el, component = null, isChild = false) => {
  * @param {HTMLElement} root - The containing element.
  */
 export const mountComponents = (root = document.body) => {
-  mountChildComponents(root)
+  mountComponent(root)
 }
 
 /**
- * Mount all components inside an element.
- * @param {HTMLElement} root
- */
-const mountChildComponents = root => {
-  for (const name in components) {
-    const elms = root.querySelectorAll(`[data-component='${name}']`)
-    for (let i = 0; i < elms.length; i++) {
-      const el = /** @type {HTMLElement} */ (elms[i])
-      mountComponent(el, components[name], true)
-    }
-  }
-}
-
-/**
- *
  * @param {string | HTMLElement} el An element (or an id).
  */
-export const very = el =>
+export const getInstance = el =>
   // @ts-ignore
-  (isString(el) ? document.getElementById(el) : el)?.__very
+  (isString(el) ? document.getElementById(el) : el)?.__very_instance
+
+/**
+ * @param {HTMLElement} el
+ * @returns {Refs}
+ */
+export const getRefs = el => {
+  /** @type {Refs} */
+  const refs = {}
+  walkComponent(el, el => {
+    const { ref } = el.dataset
+    if (ref) {
+      const entry = refs[ref]
+      if (!entry) {
+        refs[ref] = el
+      } else if (Array.isArray(entry)) {
+        entry.push(el)
+      } else {
+        refs[ref] = [entry, el]
+      }
+    }
+  })
+  return refs
+}
