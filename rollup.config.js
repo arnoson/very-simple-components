14b316b5a3f4445538f25c9e580c9e6bd9553fac
arnoson <hallo@arno-schlipf.info>
2021-07-06 12:21:54 +0200

    Add dev script

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,21 @@ import fileSize from 'rollup-plugin-filesize'
 import esbuild from 'rollup-plugin-esbuild'
 import dts from 'rollup-plugin-dts'
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: { file: 'dist/index.es.js', format: 'es' },
-    plugins: [
-      esbuild({
-        minify: process.env.NODE_ENV === 'production'
-      }),
-      fileSize({ showMinifiedSize: false, showBrotliSize: true })
-    ]
-  },
-  {
-    input: './src/index.ts',
-    output: [{ file: 'dist/types.d.ts', format: 'es' }],
-    plugins: [dts()]
-  }
-]
+const isProduction = process.env.NODE_ENV === 'production'
+
+const types = {
+  input: './src/index.ts',
+  output: [{ file: 'dist/types.d.ts', format: 'es' }],
+  plugins: [dts()]
+}
+
+const bundle = {
+  input: 'src/index.ts',
+  output: { file: 'dist/index.es.js', format: 'es' },
+  plugins: [
+    esbuild({ minify: isProduction }),
+    fileSize({ showMinifiedSize: false, showBrotliSize: true })
+  ]
+}
+
+export default isProduction ? [bundle, types] : bundle
