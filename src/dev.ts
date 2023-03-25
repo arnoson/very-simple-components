067e2875aa221c4f4444c37f8b8ed7a874e955d5
arnoson <hallo@arno-schlipf.info>
2023-03-25 14:07:05 +0100

    chore: update dev example

--- src/dev.ts
+++ src/dev.ts
@@ -1,17 +1,36 @@
-import { registerComponent, mountComponents } from '.'
+import {
+  registerComponent,
+  mountComponents,
+  SimpleElement,
+  defineOptions
+} from '.'
 
-const options = {
-  props: {
-    number: 42,
-    string: 'default',
-    bool: false,
-    array: () => [],
-    obj: () => ({})
-  }
-}
-
-registerComponent('test', options, ({ el, props }) => {
-  console.log(props.bool, el.dataset.bool)
+const options = defineOptions({
+  props: { count: 0 },
+  events: { count: Number }
 })
+
+const component = registerComponent('counter', options, ctx => {
+  const { el, props, ComponentEvent } = ctx
+  const { count, countDisplay } = ctx.refs
+
+  const update = (value: number) => (countDisplay!.innerText = `${value}`)
+
+  count?.addEventListener('click', () => {
+    update(++props.count)
+    el.dispatchEvent(new ComponentEvent('count', props.count))
+  })
+
+  update(props.count)
+})
+
+type ComponentElement = SimpleElement<typeof component>
+const el = document.querySelector<ComponentElement>(
+  '[data-simple-component="counter"]'
+)!
+
+el.addEventListener('count', ({ detail: count }) =>
+  console.log(`Count: ${count}`)
+)
 
 mountComponents()
